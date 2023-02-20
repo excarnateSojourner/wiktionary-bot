462dad0fccd0a15ae08d30759039182dfa03e58c
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2023-02-20 15:08:43 -0800

    Improve / debug recategorize.py.

--- recategorize.py
+++ recategorize.py
@@ -24,31 +24,42 @@ def main():
 
 	site = pywikibot.Site()
 	existing_cat = pywikibot.Category(site, args.existing_cat)
-	gen = pywikibot.pagegenerators.CategorizedPageGenerator(existing_cat)
+	new_cat = None
+	if args.new_cat:
+		new_cat = pywikibot.Category(site, args.new_cat)
+	elif args.action == 'add':
+		raise ValueError('You must specify which category to add.')
+	elif args.action == 'replace':
+		raise ValueError('You must specify which category to replace "{args.existing_cat}" with.')
 
 	count = 0
-	for page in gen:
+	for page in existing_cat.members():
 		if 0 <= args.limit <= count:
 			break
-		print(f'DEBUG: processing "{page.title()}"')
+		cats = set(pywikibot.textlib.getCategoryLinks(page.text, site=site))
 		if args.action == 'add':
-			page.text = pywikibot.textlib.replaceCategoryLinks(page.text, [args.new_cat], site=site, add_only=True)
+			if new_cat in cats:
+				print(f'"{page.title()}" is already in "{args.new_cat}", so I\'m skipping it.')
+			else:
+				page.text = pywikibot.textlib.replaceCategoryLinks(page.text, [args.new_cat], site=site, add_only=True)
 			if args.dry_run:
 				print(f'Would add "{page.title()}" to category "{args.new_cat}".')
 		elif args.action == 'remove':
-			cats = pywikibot.textlib.getCategoryLinks(page.text, site=site)
-			cats.remove(pywikibot.Category(site, args.existing_cat))
+			try:
+				cats.remove(exisiting_cat)
+			except KeyError:
+				print(f'Warning: "{page.title()}" does not contain a category link that causes it to be in "{args.existing_cat}", so I\'m skipping it.')
 			page.text = pywikibot.textlib.replaceCategoryLinks(page.text, cats, site=site)
 			if args.dry_run:
 				print(f'Would save "{page.title()}" with categories: {cats}')
 		# the only other possiblility
 		elif args.action == 'replace':
-			if args.new_cat:
-				new_cat = pywikibot.Category(site, args.new_cat)
+			if new_cat in cats:
+				print(f'"{page.title()}" is already in "{args.new_cat}", so I will just remove it from "{args.existing_cat}".')
+				page.text = pywikibot.textlib.replaceCategoryInPlace(page.text, existing_cat, newcat=None)
 			else:
-				raise ValueError('You tried to replace a category, but did not specify what other category to replace it with.')
-			# sort key is preserved
-			page.text = pywikibot.textlib.replaceCategoryInPlace(page.text, existing_cat, new_cat)
+				# sort key is preserved
+				page.text = pywikibot.textlib.replaceCategoryInPlace(page.text, existing_cat, new_cat)
 			if args.dry_run:
 				print(f'Would change category "{args.existing_cat}" to "{args.new_cat}" in "{page.title()}".')
 		if not args.dry_run:
