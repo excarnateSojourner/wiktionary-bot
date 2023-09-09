ab31bec7ab0cbe0d5876caa516efceb99384181d
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2023-09-09 13:31:06 -0700

    taos_apostrophes: Record skipped entries.
    Save a list of skipped entries to a file for latter manual review.

--- taos_apostrophes.py
+++ taos_apostrophes.py
@@ -12,7 +12,7 @@ import wikitextparser
 
 quote_mark = '\N{RIGHT SINGLE QUOTATION MARK}'
 mod_letter = '\N{MODIFIER LETTER APOSTROPHE}'
-move_summary = 'Replace curly apostrophes (U+2019) with modifier letter apostrophes (U+02BC) per [[Wiktionary:Requests for moves, mergers and splits#Entries in CAT:Taos lemmas with curly apostrophes|discussion]].'
+move_summary = 'Replace curly quotes (U+2019) with modifier letter apostrophes (U+02BC) ([[Wiktionary:Requests for moves, mergers and splits#Entries in CAT:Taos lemmas with curly apostrophes|discussion]] | [https://github.com/excarnateSojourner/wiktionary-bot/blob/master/taos_apostrophes.py script]).'
 text_summary = move_summary
 category_names = ['Taos lemmas', 'Taos non-lemma forms', 'Taos noun forms']
 
@@ -24,45 +24,46 @@ def main():
 
 	site = pywikibot.Site()
 	page_generators = [pywikibot.pagegenerators.CategorizedPageGenerator(pywikibot.Category(site, name)) for name in category_names]
-	for i, page in enumerate(itertools.chain.from_iterable(page_generators)):
-		if 0 < args.limit <= i:
-			print(f'Limit reached.')
-			break
+	with open('taos_skipped.txt', 'w') as skipped_file:
+		for i, page in enumerate(itertools.chain.from_iterable(page_generators)):
+			if 0 < args.limit <= i:
+				print(f'Limit reached.')
+				break
 
-		parsed_page = wikitextparser.parse(page.text)
-		sections = parsed_page.get_sections(level=2)
-		if len(sections) == 1 and sections[0].title.strip() == 'Taos':
-			# Replace in page title
-			if quote_mark in page.title():
-				new_title = page.title().replace(quote_mark, mod_letter)
-				if args.dry_run:
-					print(f'Would move {page.title(as_link=True)} to [[{new_title}]].')
-				else:
-					print(f'Moving {page.title(as_link=True)} to [[{new_title}]].')
-					page.move(new_title, reason=move_summary)
-
-			# Replace in page text
-			section_lines = sections[0].contents.splitlines()
-			section_sub_count = 0
-			print(f'Reading {page.title(as_link=True)}...')
-			for j, line in enumerate(section_lines):
-				section_lines[j], line_sub_count = re.subn(f'(?<=\\w){quote_mark}(?=\\w)', mod_letter, line)
-				if line_sub_count:
-					print(f'Before: ' + line.encode('unicode-escape').decode())
-					print(f' After: ' + section_lines[j].encode('unicode-escape').decode())
-					section_sub_count += line_sub_count
-			if section_sub_count:
-				sections[0].contents = '\n'.join(section_lines)
-				page.text = str(parsed_page)
-				save = input(f'Save changes?\n==> ')
-				if save.casefold().startswith('y'):
+			parsed_page = wikitextparser.parse(page.text)
+			sections = parsed_page.get_sections(level=2)
+			if len(sections) == 1 and sections[0].title.strip() == 'Taos':
+				# Replace in page title
+				if quote_mark in page.title():
+					new_title = page.title().replace(quote_mark, mod_letter)
 					if args.dry_run:
-						with open(f'{i}-{page.title()}.wiki', 'w') as saveFile:
-							saveFile.write(page.text)
+						print(f'Would move {page.title(as_link=True)} to [[{new_title}]].')
 					else:
-							page.save(summary=text_summary, botFlag=True, quiet=False)
-		else:
-			print(f'Error: {page.title(as_link=True)} had an unexpected format. Skipped.')
+						print(f'Moving {page.title(as_link=True)} to [[{new_title}]].')
+						page.move(new_title, reason=move_summary)
+
+				# Replace in page text
+				section_lines = sections[0].contents.splitlines()
+				section_sub_count = 0
+				print(f'Reading {page.title(as_link=True)}...')
+				for j, line in enumerate(section_lines):
+					section_lines[j], line_sub_count = re.subn(f'(?<=\\w){quote_mark}(?=\\w)', mod_letter, line)
+					if line_sub_count:
+						print(f'Before: ' + line.encode('unicode-escape').decode())
+						print(f' After: ' + section_lines[j].encode('unicode-escape').decode())
+						section_sub_count += line_sub_count
+				if section_sub_count:
+					sections[0].contents = '\n'.join(section_lines)
+					page.text = str(parsed_page)
+					save = input(f'Save changes?\n==> ')
+					if save.casefold().startswith('y'):
+						if args.dry_run:
+							with open(f'{i}-{page.title()}.wiki', 'w') as saveFile:
+								saveFile.write(page.text)
+						else:
+								page.save(summary=text_summary, botFlag=True, quiet=False)
+			else:
+				print(page.title(), file=skipped_file)
 
 if __name__ == '__main__':
 	main()
