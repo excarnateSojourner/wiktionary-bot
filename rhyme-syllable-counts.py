208e909fbb19008ea760fbd37e163adaff5feafd
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-06-10 22:02:18 -0700

    Rhyme syllable counts v2.1
    Fix typos, store sets of pages in dict not list, and print
    more when verbose.

--- rhyme-syllable-counts.py
+++ rhyme-syllable-counts.py
@@ -5,6 +5,7 @@ import re
 import pywikibot
 import pywikibot.pagegenerators
 
+DRY_RUN_LIMIT = 10 ** 3
 TEMP_PARAMS_PATTERN = r'(\|(q\d*=)?[^=|}' + '\n' + r']*)+'
 
 def main():
@@ -15,32 +16,34 @@ def main():
 	args = parser.parse_args()
 
 	site = pywikibot.Site()
-	cats = []
+	cats = {}
 	for syllable_count in range(1, (2 if args.dry_run else 20)):
 		cat = pywikibot.Category(site, f'Category:English {syllable_count}-syllable words')
-		gen = pywikibot.pagegenerators.CategorizedPageGenerator(exclude_cat)
-		cats.append([page.title() for page in (itertools.islice(gen, 60) if args.dry_run else gen)])
+		gen = pywikibot.pagegenerators.CategorizedPageGenerator(cat)
+		cats[syllable_count] = {page for page in (itertools.islice(gen, DRY_RUN_LIMIT) if args.dry_run else gen)}
 
 # We want to exclude any terms that fall in multiple "English N-syllable words" categories.
 	if args.verbose:
 		print('Excluding entries that are in multiple categories:')
-	for cat in cats:
-		for other in cats:
-			if args.verbose:
-				for page in cat & other:
-					cat.discard(page)
-					print(page)
-			else:
-				cat -= other
+	for cat in cats.values():
+		for other in cats.values():
+			if other != cat:
+				if args.verbose:
+					for page in cat & other:
+						cat.discard(page)
+						print(page.title())
+				else:
+					cat -= other
 
 	if args.verbose:
-		print('Adding syllable counts:')
-hits = 0
-	for cat_i, cat in enumerate(cats):
-		for page_i, page in enumerate(cat):
+		print('Adding syllable counts. Periods represent pages for which no action was taken.')
+	hits = 0
+	for syllable_count, cat in cats.items():
+		if args.verbose:
+			print(f'{syllable_count}-syllable words:')
+		for page in cat:
 			if 0 < args.limit <= hits:
 				break
-			syllable_count = cat_i + 1
 			if re.fullmatch(r'[a-z]+', page.title(), flags=re.IGNORECASE):
 				page.text, page_hits = re.subn(r'^(\*+ {{rhymes?\|en' + TEMP_PARAMS_PATTERN + r')}}$', r'\1|s=' + str(syllable_count) + r'}}', page.text, flags=re.MULTILINE)
 				if page_hits:
@@ -52,6 +55,10 @@ hits = 0
 						page.save(summary='Add syllable counts to English rhymes ([[Wiktionary:Beer parlour/2024/April#Copying rhyme syllable counts from existing categories|discussion]]).', botflag=True)
 					if args.verbose:
 							print(f'Added syllable count of {syllable_count} to "{page.title()}".')
+				elif args.verbose:
+					print('.', end='')
+	if args.verbose:
+		print()
 
 if __name__ == '__main__':
 	main()
