6ddbcf9fe1429283745ae16b54ae6de6feee707e
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-11-26 13:50:35 -0800

    Categorize quote temps similarly to ref temps
    Create a new script to categorize prefixed quotation templates
    similar to how prefixed_ref_temps categorizes prefixed reference
    templates.

---
+++ prefixed_quote_temps.py
@@ -0,0 +1,33 @@
+import pywikibot
+
+DRY_RUN = False
+LIMIT = 1000
+DOC_TEXT = '''{{documentation subpage}}
+{{documentation needed}}<!-- Replace this with a short description of the purpose of the template, and how to use it. -->
+
+<includeonly>
+{{quotecat}}
+</includeonly>
+'''
+
+def main():
+	with open('prefixed_quote_temps.txt', encoding='utf-8') as temps_file:
+		quote_temps = temps_file.read().splitlines()
+
+	site = pywikibot.Site()
+	for temp_title in quote_temps[:LIMIT]:
+		doc_title = temp_title + '/documentation'
+		doc_page = pywikibot.Page(site, doc_title)
+		if doc_page.exists():
+			print(f'Warning: {doc_title} already exists')
+			continue
+		doc_page.text = DOC_TEXT
+		if DRY_RUN:
+			with open(temp_title.removeprefix('Template:').replace(':', '-') + '.txt', 'w', encoding='utf-8') as out_file:
+				out_file.write(doc_page.text)
+		else:
+			doc_page.save(summary='Categorize prefixed quotation templates', bot=True)
+			pywikibot.Page(site, temp_title).touch()
+
+if __name__ == '__main__':
+	main()
