9ed31e595358050f3a6c3fd9a2231acb15f95fbb
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2025-02-14 16:53:30 -0800

    lang_cons: Update backlinks of subpages
    After moving subpages, update their backlinks along with the backlinks of the
    parent page.

--- language_considerations.py
+++ language_considerations.py
@@ -1,4 +1,5 @@
 import argparse
+import collections.abc
 import difflib
 import re
 
@@ -46,21 +47,16 @@ def main():
 		if any(word[0].islower() for word in lang.split()):
 			print(f'Note: Skipping [[{title}]] because its language would not be titlecased.')
 			continue
-		new_title = 'Wiktionary:' + lang + ' entry guidelines'
-		backlinks = [bl for bl in page.backlinks(follow_redirects=False) if is_backlink_problematic(bl.title(), lang)]
-		if backlinks:
-			print(f'\nStopping at [[{title}]] because it has the following backlinks:')
-			for bl in backlinks[:BACKLINK_DISPLAY_MAX]:
-				print(f'\t{bl.title()}')
-			if len(backlinks) > BACKLINK_DISPLAY_MAX:
-				print(f'Warning: {len(backlinks) - BACKLINK_DISPLAY_MAX} more backlinks not shown.')
-			print('What now? (m = move it and and update backlinks; s = skip; q = quit)')
-			action = input('==> ').casefold()
-			if action.startswith('s'):
-				continue
-			# If quit or invalid action chosen
-			if not action.startswith('m'):
-				return
+
+		new_title = f'Wiktionary:{lang} entry guidelines'
+		backlinks = get_and_print_backlinks(page, lang)
+		print('What now? (m = move it and and update backlinks; s = skip; q = quit)')
+		action = input('==> ').casefold()
+		if action.startswith('s'):
+			continue
+		# If quit or invalid action chosen
+		if not action.startswith('m'):
+			return
 
 		if args.dry_run:
 			print(f'Would move [[{title}]] to [[{new_title}]].')
@@ -77,13 +73,16 @@ def main():
 		move_count += 1
 
 		# Update backlinks
-		for bl in backlinks:
-			bl_title = bl.title()
-			if (bl_title.startswith('Template:') or bl_title.startswith('Module:')) and not bl_title.endswith('/documentation'):
-				print(f'Warning: [[{bl_title}]] links to [[{title}]], but I am NOT going to touch it since it\'s a template or module.')
-				continue
-			is_lang_code_redirect = bool(re.fullmatch(r'Wiktionary:A[A-Z]{2,3}(-[A-Z]{3})?', bl_title))
-			update_links(bl, title, new_title, skip_confirmation=is_lang_code_redirect, dry_run=args.dry_run)
+		for link_target_title, links in backlinks.items():
+			subpage_part = link_target_title.partition('/')[2]
+			new_link_target_title = f'{new_title}/{subpage_part}' if subpage_part else new_title
+			for bl in links:
+				bl_title = bl.title()
+				if (bl_title.startswith('Template:') or bl_title.startswith('Module:')) and not bl_title.endswith('/documentation'):
+					print(f'Warning: [[{bl_title}]] links to [[{link_target_title}]], but I am NOT going to touch it since it\'s a template or module.')
+					continue
+				is_lang_code_redirect = bool(re.fullmatch(r'Wiktionary:A[A-Z]{2,3}(-[A-Z]{3})?', bl_title))
+				update_links(bl, link_target_title, new_link_target_title, skip_confirmation=is_lang_code_redirect, dry_run=args.dry_run)
 
 		# Remove redundant sort key
 		wikitext = wikitextparser.parse(new_page.text)
@@ -98,19 +97,32 @@ def main():
 		cat_link.string = f'[[{LANG_CONS_CAT_TITLE}]]'
 		lang_lower = lang.casefold()
 		# Middle Dutch had "Dutch, Middle" as its sort key, which should have been preserved
-		if old_sort_key == lang:
+		if old_sort_key.rstrip() == lang:
 			pywikibot_helpers.edit(new_page, wikitext.string, SORT_KEY_SUMMARY, skip_confirmation=True, dry_run=args.dry_run, indent='\t')
 		else:
 			print(f'Note: The sort key used at [[{new_title}]] is "{old_sort_key}", which does not match the language ({lang}), so I am NOT going to attempt to remove the sort key.')
-		print()
 
 		# Confirm that all backlinks have been addressed
-		remaining_backlinks = list(page.backlinks(follow_redirects=False))
-		print(f'The following backlinks to [[{title}]] remain:')
-		for bl in remaining_backlinks[:BACKLINK_DISPLAY_MAX]:
-			print(f'\t{bl.title()}')
-		if len(remaining_backlinks) > BACKLINK_DISPLAY_MAX:
-			print(f'Warning: {len(remaining_backlinks) - BACKLINK_DISPLAY_MAX} more backlinks not shown.')
+		get_and_print_backlinks(page, lang)
+		print()
+
+def get_and_print_backlinks(parent_page: pywikibot.Page, lang: str) -> dict[str, list[pywikibot.Page]]:
+	'''Looks up, prints, and returns backlinks of the specified page and all its subpages.'''
+	page_with_subpages = [parent_page]
+	page_with_subpages.extend(pywikibot.pagegenerators.PrefixingPageGenerator(f'{parent_page.title()}/', site=parent_page.site))
+	backlinks: dict[str, list[pywikibot.Page]] = {}
+	for page in page_with_subpages:
+		page_title = page.title()
+		backlinks[page_title] = [bl for bl in page.backlinks(follow_redirects=False) if should_backlink_be_updated(bl.title(), lang)]
+		if backlinks[page_title]:
+			print(f'[[{page.title()}]] has the following relevant backlinks:')
+			for bl in backlinks[page_title][:BACKLINK_DISPLAY_MAX]:
+				print(f'\t{bl.title()}')
+			if len(backlinks[page_title]) > BACKLINK_DISPLAY_MAX:
+				print(f'Warning: {len(backlinks[page_title]) - BACKLINK_DISPLAY_MAX} more backlinks not shown.')
+		else:
+			print(f'[[{page.title()}]] has no relevant backlinks.')
+	return backlinks
 
 def update_links(page: pywikibot.Page, old_target: str, new_target: str, skip_confirmation: bool = False, dry_run: bool = False) -> None:
 	wikitext = wikitextparser.parse(page.text)
@@ -126,7 +138,7 @@ def update_links(page: pywikibot.Page, old_target: str, new_target: str, skip_co
 		print(f'\tWarning: Did NOT update the link to [[{old_target}]] at [[{page.title()}]].')
 	print()
 
-def is_backlink_problematic(backlink: str, lang: str) -> bool:
+def should_backlink_be_updated(backlink: str, lang: str) -> bool:
 	for good_prefix in ACCEPTABLE_BACKLINK_PREFIXES:
 		if backlink.startswith(good_prefix):
 			return False
